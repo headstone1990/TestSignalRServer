8e367b885d820b47a3e73792d00dc4c9bfc18596
HeadStone <feuerschwanz1990@gmail.com>
2016-11-04 00:57:02 +0300

    Add project files.

---
+++ TestSignalRServer/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(TestSignalRServer.Startup))]
+
+namespace TestSignalRServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
